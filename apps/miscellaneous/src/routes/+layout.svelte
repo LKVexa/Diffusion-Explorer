d45aed6f3eacbdb64787fead3fdfdadf94a3cf63
LK/Vexa Labs by Linear Finance <russellsmithson77@outlook.com>
2026-09-09 23:26:17 -0700

    Delete apps/miscellaneous directory

--- apps/miscellaneous/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import '@diffusion-explorer/ui/styles/base.css';
-	import '@diffusion-explorer/ui/styles/layout.css';
-	import '../app.css';
-
-	let { children } = $props();
-</script>
-
-{@render children()}
